a01c72c37d2f4b66736fc06f5f239ba1dec3d197
AGABBADI <agabbadi@omnilog.fr>
2024-06-24 15:50:44 +0200

    a simple static managing state app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,132 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// FilterableProductTable
+// ..SearchBar
+// ..ProductTable
+// ....ProductCategoryRow
+// ....ProductRow
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ProductCategoryRow = props => {
+
+  const {product} = props;
+
+
+  return (
+    <tr>
+      <th colSpan="2">
+        {product.category}
+      </th>
+    </tr>
+  );
+};
+const ProductRow = props => {
+
+  const {product} = props;
+
+  const coloredName = product.stocked? product.name : <span style={{color:"red"}}>{product.name}</span>;
+
+  return (
+    <tr>
+      <td>{coloredName}</td>
+      <td align="right">{product.price}</td>
+    </tr>
+  )
+};
+
+const ProductTable = props => {
+  const {filterText, inStockOnly, products} = props;
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach(product => {
+    if(product.name.indexOf(filterText) === -1) return;
+    if(inStockOnly && !product.stocked) return;
+    if(product.category !== lastCategory){
+    rows.push(
+      <ProductCategoryRow product={product} key={product.category} >
+      </ProductCategoryRow>);
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
+  return(
+    <table width="100%">
+      <thead>
+        <tr style = {{color:"blue"}}>
+          <th align="left">
+            Name
+          </th>
+          <th align="right">
+            Price
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  )
+}
+
+const SearchBar = props =>
+  {
+    const {filterText, inStockOnly, onFilterTextChange, onStockOnlyChange} = props;
+    return (
+      <form>
+        <input type="text" placeholder="Search..." value={filterText} onChange={event => onFilterTextChange(event.target.value)}/>
+        <p>
+          <input type="checkbox" checked={inStockOnly} onChange={event => onStockOnlyChange(event.target.checked)}/>
+          {""}
+          <span style={{color:"green", fontSize:"smaller"}}>
+            Only show products in stock
+          </span>
+        </p>
+      </form>
+    )
+  }
+
+const FilterableProductTable = props => {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+  const {products} = props;
+
+  const handleFilterTextChange = filterText => {
+    setFilterText(filterText);
+  }
+
+  const handleStockOnlyChange = inStockOnly => {
+    setInStockOnly(inStockOnly);
+  }
+  return (
+    <div style={{frontFamily:"sans-serif"}}>
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly}
+        onFilterTextChange={handleFilterTextChange}
+        onStockOnlyChange={handleStockOnlyChange}
+        />
+      <ProductTable 
+        products={products} 
+        filterText={filterText} 
+        inStockOnly={inStockOnly}
+      />
+    </div>
+  );
+}
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
+
+
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById('root')
+)
